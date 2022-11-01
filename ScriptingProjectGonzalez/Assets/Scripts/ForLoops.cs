910695cb9da39c7e5fa9bf5f21fd53ee1f4a853c
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2022-11-01 09:56:38 -0700

    Coded stuff
    weeee

---
+++ ScriptingProjectGonzalez/Assets/Scripts/ForLoops.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoops : MonoBehaviour
+{
+    int numEnemies = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+
+    }
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ScriptingProjectGonzalez/Assets/Scripts/Whileloop.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whileloop : MonoBehaviour
+{
+    int cupsInTheSink = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(cupsInTheSink > 0)
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
