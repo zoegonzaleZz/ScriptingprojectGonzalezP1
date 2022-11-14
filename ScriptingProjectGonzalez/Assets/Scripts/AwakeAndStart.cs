2aa78da7c539669df16aa554bb78a346b249f680
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2022-11-14 08:54:34 -0800

    Awake and Start
    ok

---
+++ ScriptingProjectGonzalez/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
